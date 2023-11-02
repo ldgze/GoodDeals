3abf9799a0ce64c249df268c65b88bbf681f328a
Chelsea <chelseacui211@gmail.com>
2023-11-02 11:56:45 -0700

    create Vite and connect to backend server

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 import express from "express";
 import myDB from "./database/db.js";
-// import quizRoutes from "./routes/quizRoutes.js";
-// import responseRoutes from "./routes/responseRoutes.js";
+import path, { dirname } from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import { fileURLToPath } from "url";
+import indexRouter from "./routes/index.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("frontend"));
+app.use(logger("dev"));
 app.use(express.json());
-// app.use("/api/quizzes", quizRoutes);
-// app.use("/api/responses", responseRoutes);
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
+app.use("/", indexRouter);
+
 
 const startServer = async () => {
   await myDB.connect();
@@ -19,3 +28,5 @@ const startServer = async () => {
 };
 
 startServer();
+
+export default app;
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import myDB from "../database/db.js";
+
+const router = express.Router();
+
+
+
+
+
+
+
+export default router;
