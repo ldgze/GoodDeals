731fb880325f45adee0ca3f60338d8c9cf482352
Chelsea <chelseacui211@gmail.com>
2023-11-03 10:50:22 -0700

    update files

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
 import express from "express";
-import myDB from "./database/db.js";
 import path, { dirname } from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import { fileURLToPath } from "url";
+
 import indexRouter from "./routes/index.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const app = express();
-const PORT = 3000;
+let app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "frontend", "dist")));
+app.use(express.static(path.join(__dirname, "front", "dist")));
+
 app.use("/", indexRouter);
-
-
-const startServer = async () => {
-  await myDB.connect();
-  app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
-  });
-};
-
-startServer();
 
 export default app;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 import express from "express";
-import myDB from "../database/db.js";
+let router = express.Router();
 
-const router = express.Router();
-
-
+import myDB from "../db/myMongoDB.js";
 
 
+/* GET home page. */
+router.get("/api/deals", async function (req, res) {
 
-
+  const photos = await myDB.getPhotos();
+  res.json({photos});
+});
 
 export default router;
