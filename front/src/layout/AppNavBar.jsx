ef490cc7accf8478e650dd7e314d69459e4a5eeb
zexigong <gong.zex@northeastern.edu>
2023-11-03 18:06:47 -0700

    update navbar

--- front/src/layout/AppNavBar.jsx
+++ front/src/layout/AppNavBar.jsx
@@ -2,62 +2,21 @@ import "../asset/style/AppNavBar.css";
 
 export function AppNavBar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
+    <nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="#">Grocery</a></li>
+        <li><a href="#">Beauty</a></li>
+        <li><a href="#">Fashion</a></li>
+        <li><a href="#">Electronic</a></li>
+        <li>
+            <div class="search-box">
+            <input type="search"
+              placeholder="Search"/>
+                    <button class="search-button">Search</button>
+                </div>
             </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Grocery
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Beauty
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Fashion
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Electronic
-              </a>
-            </li>
-          </ul>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
-      </div>
-    </nav>
+    </ul>
+</nav>
   );
 }
