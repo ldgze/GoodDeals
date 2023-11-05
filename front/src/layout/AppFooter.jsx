0df32a9f9a131d4359f5692d204643ee0e6c907a
Chelsea <chelseacui211@gmail.com>
2023-11-05 11:49:32 -0800

    update footer and homepage

--- front/src/layout/AppFooter.jsx
+++ front/src/layout/AppFooter.jsx
@@ -1,3 +1,9 @@
 export function AppFooter() {
-  return <footer>&copy; 2023 Yushi Cui & Zexi Gong</footer>;
+  return (
+    <footer className="footer mt-auto py-3">
+      <div className="container text-center">
+        <span className="text-muted">&copy; 2023 Yushi Cui & Zexi Gong</span>
+      </div>
+    </footer>
+  );
 }
--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -9,16 +9,23 @@ import { useState, useEffect } from "react";
 
 export function HomePage() {
   return (
-      <div>
-        <AppNavBar />
-        <h1>GoodDeals - Discover and Share Deals</h1>
-        <Link to="/createdeal">
-        <button type="button">Create a Deal</button>
-        </Link>
-        <AppFooter />
+    <div>
+      <AppNavBar />
+      <div className="container mt-4">
+        <div className="row justify-content-center">
+          <div className="col-md-8 text-center">
+            <h1>GoodDeals - Discover and Share Deals</h1>
+            <p className="lead">The best place to find and share deals online.</p>
+            <Link to="/createdeal" className="btn btn-primary btn-lg">
+              Create a Deal
+            </Link>
+          </div>
+        </div>
       </div>
-    );
-  }
+      <AppFooter />
+    </div>
+  );
+}
 
   // let [deals, setDeals] = useState([]);
 
