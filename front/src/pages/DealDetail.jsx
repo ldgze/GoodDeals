b5e17f3ecea40722499bc79aad707ef8f553c5a4
zexigong <gong.zex@northeastern.edu>
2023-11-05 20:56:34 -0800

    Update DealDetail.jsx

--- front/src/pages/DealDetail.jsx
+++ front/src/pages/DealDetail.jsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from 'react';
-<<<<<<< Updated upstream
-import { useParams } from 'react-router-dom';
 import { ButtonLike } from '../components/ButtonLike';
-=======
 import { useParams, Link } from 'react-router-dom';
->>>>>>> Stashed changes
+
 
 export function DealDetail() {
   const [deal, setDeal] = useState(null);
@@ -30,29 +27,20 @@ export function DealDetail() {
   }
 
   return (
-<<<<<<< Updated upstream
-    <div>
-      <h1>{deal.title}</h1>
-      <p><strong>Description:</strong> {deal.description}</p>
-      <p><strong>Web Link:</strong> <a href={deal.weblink} target="_blank" rel="noopener noreferrer">{deal.weblink}</a></p>
-      <p><strong>Image:</strong> <img src={deal.imagelink} alt={deal.title} /></p>
-      <p><strong>Category:</strong> {deal.category}</p>
-      <ButtonLike deal = {deal}/>
-=======
     <div className="container my-5">
       <div className="card">
         <img src={deal.imagelink} alt={deal.title} className="card-img-top" style={{ maxHeight: '400px', objectFit: 'cover' }} />
         <div className="card-body">
           <h1 className="card-title">{deal.title}</h1>
           <p className="card-text">{deal.description}</p>
-          <a href={deal.weblink} className="btn btn-primary">Visit Deal</a>
           <Link to={`/edit/${dealId}`} className="btn btn-secondary mx-2">Edit</Link>
           <button onClick={() => { if (window.confirm('Are you sure you want to delete this deal?')) { /* implement deletion logic */ }}} className="btn btn-danger">Delete</button>
-          <button onClick={() => { /* implement like logic */ }} className="btn btn-success mx-2">Like ({deal.like})</button>
+          <ButtonLike deal = {deal}/>
         </div>
       </div>
->>>>>>> Stashed changes
-    </div>
+     </div> 
+
+
   );
 }
 
